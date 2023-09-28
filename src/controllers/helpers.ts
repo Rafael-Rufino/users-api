08e0071daa86ab13da8f6c0ec73021afbfdde636
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-28 01:36:04 -0300

    feat: add errors messagens

---
+++ src/controllers/helpers.ts
@@ -0,0 +1,34 @@
+export const ok = (body: any) => ({
+  statusCode: 200,
+  body,
+})
+
+export const created = (body: any) => ({
+  statusCode: 201,
+  body,
+})
+
+export const badRequest = (message: string) => ({
+  statusCode: 400,
+  body: message,
+})
+
+export const unauthorized = (message: string) => ({
+  statusCode: 401,
+  body: message,
+})
+
+export const forbidden = (message: string) => ({
+  statusCode: 403,
+  body: message,
+})
+
+export const notFound = (message: string) => ({
+  statusCode: 404,
+  body: message,
+})
+
+export const serverError = (message: string) => ({
+  statusCode: 500,
+  body: message,
+})
