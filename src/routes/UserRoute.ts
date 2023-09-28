52554503f0b916d24bb73868ab0bdf45c693635f
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-28 00:17:30 -0300

    feat: add to update

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { CreateUserController } from '../controllers/create-user/create-user'
 import { GetUsersController } from '../controllers/get-users/get-users'
+import { UpdateUserController } from '../controllers/update-user/update.user'
 import { MongoCreateUserRepository } from '../repositories/create-user/mongo-create-user'
 import { MongoGetUsersRepository } from '../repositories/get-users/mongo-get-users'
+import { MongoUpdateUserRepository } from '../repositories/update-user/mongo-update-user'
 
 const router = Router()
 
@@ -31,6 +33,19 @@ router.post('/users', async (req, res) => {
   const response = res.status(statusCode).send(body)
 
   return response
+})
+
+router.patch('/users/:id', async (req, res) => {
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository()
+  const updateUserController = new UpdateUserController(
+    mongoUpdateUserRepository
+  )
+  const { body, statusCode } = await updateUserController.handle({
+    body: req.body,
+    params: req.params,
+  })
+
+  return res.status(statusCode).send(body)
 })
 
 export default router
